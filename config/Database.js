46610269a8820c354797b579921c3be4c09f3772
PutraSaskara <saskarap@gmail.com>
2024-03-02 00:42:33 +0800

    update database and vercel json

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('demo','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize('personal_menwishfew','personal_menwishfew','5975180a8f3e376dc2b84e81ac96001327162fc9',{
+    host: '3dg.h.filess.io',
+    dialect: 'mysql',
+    port: '3307'
 });
 
 module.exports = db;
