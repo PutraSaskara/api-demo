25fc643768e1006b9d1b995f4c5150198670c1cf
PutraSaskara <saskarap@gmail.com>
2024-03-02 00:51:35 +0800

    update database module

--- config/Database.js
+++ config/Database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize('personal_menwishfew','personal_menwishfew','5975180a8f3e376dc2b84e81ac96001327162fc9',{
     host: '3dg.h.filess.io',
     dialect: 'mysql',
+    dialectModule: require('mysql2'),
     port: '3307'
 });
 
