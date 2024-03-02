7928727632b3aff80968cba06b55a99c7f950d08
PutraSaskara <saskarap@gmail.com>
2024-03-02 08:31:37 +0800

    freedatabase

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,27 @@
+// filess.io
+
+// const { Sequelize } = require("sequelize");
+// const mysql2 = require('mysql2');
+
+// const db = new Sequelize('personal_menwishfew','personal_menwishfew','5975180a8f3e376dc2b84e81ac96001327162fc9',{
+//     host: '3dg.h.filess.io',
+//     dialect: 'mysql',
+//     dialectModule: mysql2,
+//     port: '3307'
+// });
+
+// module.exports = db;
+
+// freesqldatabas
+
 const { Sequelize } = require("sequelize");
 const mysql2 = require('mysql2');
 
-const db = new Sequelize('personal_menwishfew','personal_menwishfew','5975180a8f3e376dc2b84e81ac96001327162fc9',{
-    host: '3dg.h.filess.io',
+const db = new Sequelize('sql6687828','sql6687828','w8wSEufqNR',{
+    host: 'sql6.freesqldatabase.com',
     dialect: 'mysql',
     dialectModule: mysql2,
-    port: '3307'
+    port: '3306'
 });
 
 module.exports = db;
